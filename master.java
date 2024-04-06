e59e27266b1b85dcfbd9b66a8234d48113ff1bfa
Rafal <rafalgad@wp.pl>
2024-04-06 11:27:51 +0200

    pierwszy commit

--- master.java
+++ master.java
@@ -4,10 +4,27 @@
         int studentCode = 51294;
         System.out.println(fullName + " " + studentCode);
 
+        int a = 5;
+        int b = 4;
+        int suma = a + b;
+        System.out.println( a + " + " + b + " = " + suma );
+
+        int[] tablica = {1,5,4,2};
+        
+        int x = 0;
+        for (int i = 0; i < tablica.length; i++) { 
+            x += tablica[i];
+        }
+        int srednia = x/tablica.length;
+        System.out.println("srednia wynosi: " + srednia);
+
+        int number = 14;
+        if (number % 2 == 0) {
+            System.out.println("parzysta");
+        } else {
+            System.out.println("nieparzysta");
+        }
         
     }
-}
-class klasa {
-    
 }
  
